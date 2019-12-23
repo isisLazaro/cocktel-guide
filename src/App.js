a223c7b6583f2869ebff0cb5ffdc377f1dd5577c
Isis Lázaro <isislazaro@gmail.com>
2019-12-23 10:12:39 -0600

    delete comments in app.js

--- src/App.js
+++ src/App.js
@@ -12,16 +12,7 @@ class App extends Component {
   state = {
     cocktails: []
   };
-  /* getData = async e => {
-    e.preventDefault();
-    const cocktailName = e.target.elements.cocktailName.value;
-    const api_call = await fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`
-    );
-    const data = await api_call.json();
-    this.setState({ cocktails: data.drinks });
-    console.log(this.state.cocktails);
-  }; */
+
   getData = async event => {
     event.preventDefault();
     const cocktailName = event.target.elements.cocktailName.value;
@@ -44,15 +35,6 @@ class App extends Component {
             <Cocktails cocktails={this.state.cocktails} />
           </div>
         </div>
-
-        {/* {this.state.cocktails.map(cocktail => {
-          return (
-            <div key={cocktail.idDrink}>
-              <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink}></img>
-              <p>{cocktail.strDrink}</p>
-            </div>
-          );
-        })} */}
       </div>
     );
   }
