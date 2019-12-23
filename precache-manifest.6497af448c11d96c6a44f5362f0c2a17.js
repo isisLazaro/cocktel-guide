08742f2ac4695463fcf743cda600214dd42f73aa
Isis Lázaro <isislazaro@gmail.com>
2019-12-23 17:51:06 -0600

    Updates

---
+++ precache-manifest.6497af448c11d96c6a44f5362f0c2a17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf4da2eb7530b4eb8ceece2df3c8d82",
+    "url": "/cocktel-guide/index.html"
+  },
+  {
+    "revision": "ed8d3fd15ab8fcbec978",
+    "url": "/cocktel-guide/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "26b20ffa950d523e7cd0",
+    "url": "/cocktel-guide/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed8d3fd15ab8fcbec978",
+    "url": "/cocktel-guide/static/js/2.4dc5267b.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/cocktel-guide/static/js/2.4dc5267b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26b20ffa950d523e7cd0",
+    "url": "/cocktel-guide/static/js/main.da6fa73e.chunk.js"
+  },
+  {
+    "revision": "44fe114d979b56803ffc",
+    "url": "/cocktel-guide/static/js/runtime-main.b2877620.js"
+  }
+]);
