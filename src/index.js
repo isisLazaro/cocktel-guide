07b5feaf99bfe5bdaee26ac29d8092ab8272ea74
Isis Lázaro <isislazaro@gmail.com>
2019-12-23 10:00:30 -0600

    new file structure for router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Router from "./components/Router";
+import Router from "./router/index";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Router />, document.getElementById("root"));
